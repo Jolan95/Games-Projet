eeb44b65c4da6b95b6f124588806225a72f1f461
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-06-27 14:55:27 +0200

    new version ranking

--- ranking.php
+++ ranking.php
@@ -16,128 +16,119 @@ include "handleLanguage/lang.php";
 	<body>
 		<?php require_once 'header.php'; ?>
         <div class="ranking">
-            <div class="container">
-                <div class="row">
-	
-        <h1>
-            <?php
-             if(isset($_GET['record'])){
-                switch($_GET['record']){
-                case 'record_Ecap' : 
-                echo $capitales." /10";
-                break;
-                case 'record_Mcap' : 
-                echo $capitales." /50";
-                break;
-                case 'record_Hcap' : 
-                echo $capitalesFull;
-                break;
-                case 'record_Guess' : 
-                echo 'Guessing Number';
-                break;
-                case 'record_flappy' : 
-                echo 'Flappy Bird';
-                break;
-                default : 
-                echo $diceGame;
+            <h1 class="text-center">
+                <?php
+                 if(isset($_GET['record'])){
+                    switch($_GET['record']){
+                    case 'record_Ecap' : 
+                    echo $capitales." /10";
+                    break;
+                    case 'record_Mcap' : 
+                    echo $capitales." /50";
+                    break;
+                    case 'record_Hcap' : 
+                    echo $capitalesFull;
+                    break;
+                    case 'record_Guess' : 
+                    echo 'Guessing Number';
+                    break;
+                    case 'record_flappy' : 
+                    echo 'Flappy Bird';
+                    break;
+                    default : 
+                    echo $diceGame;
+                    }
+                } else {
+                    echo $diceGame;
                 }
-            } else {
-                echo $diceGame;
+                ?>
+            </h1>   
+            <form method="GET">
+                <label for="record-select">Classement : </label>
+                <select   name="record" id="record-select">
+                    <option >Selection</option>
+                    <option value="record_dice"><?php echo $diceGame; ?></option>
+                    <option value="record_Ecap"><?php echo $capitales."/10"; ?> </option>
+                    <option value="record_Mcap"><?php echo $capitales." /50 "; ?></option>
+                    <option value="record_Hcap"><?php echo $capitalesFull; ?></option>
+                    <option value="record_Guess">Guessing Number </option>
+                    <option value="record_flappy">Flappy Bird</option>
+                </select>  
+            </form> 
+        </div>
+        <table class="container"  id="customers">
+            <tr class="legend">
+                <th><?php echo $rank ?></th>
+                <th>Pseudo</th>
+                <th><?php echo $highScore ?></th> 
+            </tr>
+        <?php
+
+        // function to fetch desc records
+        function getRecordDesc($game){
+            $i=0;
+            $pdo = new PDO($_ENV["CLEARDB_DATABASE_DSN"], $_ENV["CLEARDB_DATABASE_USERNAME"], $_ENV["CLEARDB_DATABASE_PASSWORD"]);
+            $statement = $pdo->prepare('SELECT pseudo, '.$game.' FROM users WHERE '.$game.' > 0 ORDER BY '.$game.' DESC  LIMIT 0, 15'); 
+            if ($statement->execute()) {
+                while ($user = $statement->fetch(PDO::FETCH_ASSOC)) {
+                    $i++;
+                    if($i === 1){
+                        echo '<tr class="gold"><td>'.$i.'</td><td>'.$user['pseudo'].'</td><td>'.$user[$game].'</td></tr>';
+                    } else if ($i === 2){
+                        echo '<tr class="silver"><td>'.$i.'</td><td>'.$user['pseudo'].'</td><td>'.$user[$game].'</td></tr>';
+                    } else if ($i === 3){
+                        echo '<tr class="bronze"><td>'.$i.'</td><td>'.$user['pseudo'].'</td><td>'.$user[$game].'</td></tr>';
+                    } else {
+                    
+                        echo '<tr><td>'.$i.'</td><td>'.$user['pseudo'].'</td><td>'.$user[$game].'</td></tr>';
+                    }
+                }
+            }else {
+                echo "Aucun record trouvé";
             }
-            ?>
-       </h1>
-        
-        <form method="GET">
-            <label style="font-weight: 700; color : white" for="record-select">Classement : </label>
-            <select   name="record" id="record-select">
-                <option >Selection</option>
-                <option value="record_dice"><?php echo $diceGame; ?></option>
-                <option value="record_Ecap"><?php echo $capitales."/10"; ?> </option>
-                <option value="record_Mcap"><?php echo $capitales." /50 "; ?></option>
-                <option value="record_Hcap"><?php echo $capitalesFull; ?></option>
-                <option value="record_Guess">Guessing Number </option>
-                <option value="record_flappy">Flappy Bird</option>
-
-            </select>  
-        </form> 
-    </div>
-    <table   id="customers">
-        <tr class="legend">
-            <th><?php echo $rank ?></th>
-            <th>Pseudo</th>
-            <th><?php echo $highScore ?></th> 
-        </tr>
-    <?php
-
-function getRecordDesc($game){
-    $i=0;
-    $pdo = new PDO($_ENV["CLEARDB_DATABASE_DSN"], $_ENV["CLEARDB_DATABASE_USERNAME"], $_ENV["CLEARDB_DATABASE_PASSWORD"]);
-    $statement = $pdo->prepare('SELECT pseudo, '.$game.' FROM users WHERE '.$game.' > 0 ORDER BY '.$game.' DESC  LIMIT 0, 15'); 
-/**$statement->bindValue(':game', $game);*/
-if ($statement->execute()) {
-
-    while ($user = $statement->fetch(PDO::FETCH_ASSOC)) {
-        $i++;
-        if($i === 1){
-            echo '<tr style=" background-color: goldenrod; opacity : 0.8;"><td>'.$i.'</td><td>'.$user['pseudo'].'</td><td>'.$user[$game].'</td></tr>';
-        } else if ($i === 2){
-            echo '<tr style=" background-color: silver; opacity : 0.8;"><td>'.$i.'</td><td>'.$user['pseudo'].'</td><td>'.$user[$game].'</td></tr>';
-        } else if ($i === 3){
-            echo '<tr style=" background-color: burlywood; opacity : 0.8;"><td>'.$i.'</td><td>'.$user['pseudo'].'</td><td>'.$user[$game].'</td></tr>';
-        } else {
-
-            echo '<tr><td>'.$i.'</td><td>'.$user['pseudo'].'</td><td>'.$user[$game].'</td></tr>';
         }
-    }
-    
-}else {
-    echo "Aucun record trouvé";
-}
-}
-// Function fetch asc record
-function getRecordAsc($game){
-    $i=0;
 
-   /* $pdo = new PDO('mysql:host=eu-cdbr-west-01.cleardb.com;dbname=heroku_53ae102770f6a82', "ba3595a923b6d7", "75287824");*/
-   $pdo = new PDO($_ENV["CLEARDB_DATABASE_DSN"], $_ENV["CLEARDB_DATABASE_USERNAME"], $_ENV["CLEARDB_DATABASE_PASSWORD"]);
+        // Function fetch asc records
+        function getRecordAsc($game){
+            $i=0;
+            $pdo = new PDO($_ENV["CLEARDB_DATABASE_DSN"], $_ENV["CLEARDB_DATABASE_USERNAME"], $_ENV["CLEARDB_DATABASE_PASSWORD"]);
+            $statement = $pdo->prepare('SELECT pseudo, '.$game.' FROM users WHERE '.$game.' > 0 ORDER BY '.$game.' ASC  LIMIT 0, 15'); 
+            if ($statement->execute()) {
+            
+                while ($user = $statement->fetch(PDO::FETCH_ASSOC)) {
+                    $i++;
+                    if($i === 1){
+                        echo '<tr class="gold"><td>'.$i.'</td><td>'.$user['pseudo'].'</td><td>'.$user[$game].'</td></tr>';
+                    } else if ($i === 2){
+                        echo '<tr class="silver"><td>'.$i.'</td><td>'.$user['pseudo'].'</td><td>'.$user[$game].'</td></tr>';
+                    } else if ($i === 3){
+                        echo '<tr class="bronze"><td>'.$i.'</td><td>'.$user['pseudo'].'</td><td>'.$user[$game].'</td></tr>';
+                    } else {
+                    
+                        echo '<tr><td>'.$i.'</td><td>'.$user['pseudo'].'</td><td>'.$user[$game].'</td></tr>';
+                    }
+                }
 
-$statement = $pdo->prepare('SELECT pseudo, '.$game.' FROM users WHERE '.$game.' > 0 ORDER BY '.$game.' ASC  LIMIT 0, 15'); 
-/**$statement->bindValue(':game', $game);*/
-if ($statement->execute()) {
-
-    while ($user = $statement->fetch(PDO::FETCH_ASSOC)) {
-        $i++;
-        if($i === 1){
-            echo '<tr style=" background-color: goldenrod;"><td>'.$i.'</td><td>'.$user['pseudo'].'</td><td>'.$user[$game].'</td></tr>';
-        } else if ($i === 2){
-            echo '<tr style=" background-color: silver;"><td>'.$i.'</td><td>'.$user['pseudo'].'</td><td>'.$user[$game].'</td></tr>';
-        } else if ($i === 3){
-            echo '<tr style=" background-color: burlywood;"><td>'.$i.'</td><td>'.$user['pseudo'].'</td><td>'.$user[$game].'</td></tr>';
-        } else {
-
-            echo '<tr><td>'.$i.'</td><td>'.$user['pseudo'].'</td><td>'.$user[$game].'</td></tr>';
+            }else {
+                echo "erreur fatale";
+            }
         }
-    }
-    
-}else {
-    echo "erreur fatale";
-}
-}
-$game="record_dice";
-if(!isset($_GET['record'])){
-    getRecordDesc('record_dice');
-} else {
-    if($_GET['record'] === 'record_Guess'){
-    getRecordAsc($_GET['record']);
-    }else{
-    getRecordDesc($_GET['record']);
-    }
-}
+
+        $game="record_dice";
+        if(!isset($_GET['record'])){
+            getRecordDesc('record_dice');
+        } else {
+            if($_GET['record'] === 'record_Guess'){
+            getRecordAsc($_GET['record']);
+            }else{
+            getRecordDesc($_GET['record']);
+            }
+        }
 
 
     
-    ?>
-    </table>
+        ?>
+        </table>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script>
         $(document).ready(function(){  
@@ -147,5 +138,5 @@ if(!isset($_GET['record'])){
          })
 
     </script>
-</body>
+    </body>
 </html>
