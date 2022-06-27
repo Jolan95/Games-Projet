548f112879c158b1926a7bf9cbbd0c54224ecece
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-06-27 12:40:15 +0200

    try to fix css

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include "handleLanguage/lang.php";
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 		<link rel="stylesheet" href="assets/carousel/owl.carousel.min.css">
 		<link rel="stylesheet" href="assets/carousel/owl.theme.default.min.css">
-		<link rel="stylesheet" type="text/css" href="Style/style.css">
+		<link rel="stylesheet" type="text/css" href="style/style.css">
 	</head>
 	<body>
 		<?php require_once 'header.php'; ?>
