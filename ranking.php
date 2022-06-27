fb623dde09723a18f2264bcc2992590a936856c0
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-06-27 12:51:35 +0200

    try to fix css

--- ranking.php
+++ ranking.php
@@ -11,7 +11,7 @@ include "handleLanguage/lang.php";
 		<title>J-Games Online</title>
 		<meta name="description" content="Jeux d'arcades, quiz, jeux de dés,... Découvrez le classement individuel par jeux !" />
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-		<link rel="stylesheet" type="text/css" href="style/style.css">
+		<link rel="stylesheet" type="text/css" href="Style/style.css">
 	</head>
 	<body>
 		<?php require_once 'header.php'; ?>
@@ -19,7 +19,7 @@ include "handleLanguage/lang.php";
             <div class="container">
                 <div class="row">
 	
-        <h1 style="display : flex; justify-content: center; margin-top : 3vh; color : white">
+        <h1>
             <?php
              if(isset($_GET['record'])){
                 switch($_GET['record']){
