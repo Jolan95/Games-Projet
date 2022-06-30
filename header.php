70d3f8bbde11bb8e722fb73cd56d41a4677e3777
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-06-30 11:00:47 +0200

    new form page

--- header.php
+++ header.php
@@ -73,8 +73,8 @@ if(isset($_SESSION['id'])){
             <svg xmlns="http://www.w3.org/2000/svg" height="18px" width="18px" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-                <?php echo $_SESSION["user"]; ?> 
             </svg>
+            <?php echo $_SESSION["user"]; ?> 
         </a>
         <?php } ?>  
     </nav>    
