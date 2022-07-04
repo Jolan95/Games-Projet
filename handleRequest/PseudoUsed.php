9d3c51a19853b32bdd351b76d6e6ae44a293077d
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-07-04 11:16:24 +0200

    new form page

--- handleRequest/PseudoUsed.php
+++ handleRequest/PseudoUsed.php
@@ -2,6 +2,10 @@
 $pdo = new PDO($_ENV["CLEARDB_DATABASE_DSN"], $_ENV["CLEARDB_DATABASE_USERNAME"], $_ENV["CLEARDB_DATABASE_PASSWORD"]);
 $statement = $pdo->prepare("SELECT pseudo FROM users WHEN pseudo = :pseudo");
 $statement->bindValue(":pseudo", $_POST["pseudo"]);
-
+if($statement->execute()){
+    echo "true";
+} else {
+    echo "false";
+}
 
 ?>
