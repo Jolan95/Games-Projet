0e353dc6cbbdede85aa5c5765d87393dc6383ac4
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-07-04 12:28:47 +0200

    new form page

--- reinitializePassword.php
+++ reinitializePassword.php
@@ -19,8 +19,6 @@
 		<title>J-Games Online</title>
 		<meta name="description" content="Jeux d'arcades, quiz, jeux de dés,..." />
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel="stylesheet" href="assets/carousel/owl.carousel.min.css">
-		<link rel="stylesheet" href="assets/carousel/owl.theme.default.min.css">
         <link rel="stylesheet" type="text/css" href="Style/style.css">
 	</head>
 	<body>
