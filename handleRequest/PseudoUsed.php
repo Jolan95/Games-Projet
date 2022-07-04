74c213904c65d3cfd611ec3da9e931b72f6f0977
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-07-04 11:06:10 +0200

    new passwordreset  page

--- handleRequest/PseudoUsed.php
+++ handleRequest/PseudoUsed.php
@@ -2,10 +2,6 @@
 $pdo = new PDO($_ENV["CLEARDB_DATABASE_DSN"], $_ENV["CLEARDB_DATABASE_USERNAME"], $_ENV["CLEARDB_DATABASE_PASSWORD"]);
 $statement = $pdo->prepare("SELECT pseudo FROM users WHEN pseudo = :pseudo");
 $statement->bindValue(":pseudo", $_POST["pseudo"]);
-if($statement->execute()){
-    echo "true";
-} else {
-    echo "false";
-}
+echo "hod";
 
 ?>
