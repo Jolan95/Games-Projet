2f01131a286ea597247a796a5b8a7893169748b3
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-07-04 11:59:35 +0200

    new form page

--- passwordReset.php
+++ passwordReset.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 include "handleLanguage/lang.php";
-require_once 'Class/user.php';
+include "Class/user";
 $successRequest = 0;
 $token = $_GET['token'] ;
-require_once "handleRequest/passwordReset-verif.php";
+include "handleRequest/passwordReset-verif.php";
 // if(!$token){
 //     echo "Impossible d'accéder à cette page, une erreur est survenu!";
 //     die();
@@ -19,7 +19,7 @@ require_once "handleRequest/passwordReset-verif.php";
 		<title>J-Games Online</title>
 		<meta name="description" content="Jeux d'arcades, quiz, jeux de dés,..." />
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-		<link rel="stylesheet" type="text/css" href="style/style.css">
+		<link rel="stylesheet" type="text/css" href="Style/style.css">
 	</head>
 	<body>
 		<?php require_once 'header.php'; ?>
