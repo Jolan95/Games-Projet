f2684b9a8dda06efd3823c251b5cb7000965a790
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-07-04 11:37:09 +0200

    new form page

--- handleRequest/PseudoUsed.php
+++ handleRequest/PseudoUsed.php
@@ -1,9 +1,19 @@
 <?php
-$pdo = new PDO($_ENV["CLEARDB_DATABASE_DSN"], $_ENV["CLEARDB_DATABASE_USERNAME"], $_ENV["CLEARDB_DATABASE_PASSWORD"]);
-$statement = $pdo->prepare("SELECT pseudo FROM users WHEN pseudo = :pseudo");
-$statement->bindValue(":pseudo", $_POST["pseudo"]);
-if($statement->execute()){
-    echo "tre";
-}
+            $pdo = new PDO($_ENV["CLEARDB_DATABASE_DSN"], $_ENV["CLEARDB_DATABASE_USERNAME"], $_ENV["CLEARDB_DATABASE_PASSWORD"]);
+            $statement = $pdo->prepare('SELECT pseudo, '.$game.' FROM users WHERE '.$game.' > 0 ORDER BY '.$game.' ASC  LIMIT 0, 15'); 
+            if ($statement->execute()) {
+            
+                while ($user = $statement->fetch(PDO::FETCH_ASSOC)) {
+                    $i++;
+                    if($i === 1){
+                        echo '<tr class="gold"><td>'.$i.'</td><td>'.$user['pseudo'].'</td><td>'.$user[$game].'</td></tr>';
+                    } else {
+                    
+                        echo '<tr><td>'.$i.'</td><td>'.$user['pseudo'].'</td><td>'.$user[$game].'</td></tr>';
+                    }
+                }
 
+            }else {
+                echo "erreur fatale";
+            }
 ?>
