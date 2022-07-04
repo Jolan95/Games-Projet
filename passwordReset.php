384d03adb1e6a36589175b2deca67653cc1193ee
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-07-04 11:55:14 +0200

    new form page

--- passwordReset.php
+++ passwordReset.php
@@ -18,8 +18,8 @@ require_once "handleRequest/passwordReset-verif.php";
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>J-Games Online</title>
 		<meta name="description" content="Jeux d'arcades, quiz, jeux de dés,..." />
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href="Style/style.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	</head>
 	<body>
 		<?php require_once 'header.php'; ?>
