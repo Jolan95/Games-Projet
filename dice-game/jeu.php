db100ce4c1b5d967ee212c6a7c819fe748ca5d6e
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-07-04 19:03:25 +0200

    cleaning style dice

--- dice-game/jeu.php
+++ dice-game/jeu.php
@@ -11,7 +11,7 @@ include "../handleLanguage/lang.php";
 		<title>J-Games Online</title>
 		<meta name="description" content="Jeux d'arcades, quiz, jeux de dés,... Venez découvrir notre liste de jeux !" />
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-		<!-- <link rel="stylesheet" type="text/css" href="Style/style.css"> -->
+		<link rel="stylesheet" type="text/css" href="Style/style.css">
         <link rel="stylesheet" type="text/css" href="jeuDice.css">
 	</head>
 	<body>
