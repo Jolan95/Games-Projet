e5883aebf27173fff7bf4080f33aeae40de7f10a
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-07-04 12:45:13 +0200

    new form page

--- reinitializePassword.php
+++ reinitializePassword.php
@@ -1,11 +1,16 @@
-<!DOCTYPE html>
 <?php
- include "handleLanguage/lang.php";
- $successRequest = 0;
- $token = $_GET['token'] ;
- include "handleRequest/passwordReset-verif.php";
-
+session_start();
+include "handleLanguage/lang.php";
+include "Class/user";
+$successRequest = 0;
+$token = $_GET['token'] ;
+include "handleRequest/passwordReset-verif.php";
+if(!$token){
+    echo "Impossible d'accéder à cette page, une erreur est survenu!";
+    die();
+}
 ?>
+<!DOCTYPE html>
 <html lang="fr">
     <head>
 		<link rel="icon" type="image/png" href="Img/favicon.png">
@@ -14,7 +19,7 @@
 		<title>J-Games Online</title>
 		<meta name="description" content="Jeux d'arcades, quiz, jeux de dés,..." />
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="Style/style.css">
+		<link rel="stylesheet" type="text/css" href="Style/style.css">
 	</head>
 	<body>
 		<?php require_once 'header.php'; ?>
@@ -40,7 +45,23 @@
         </div>    
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
         <script>
-
+            $("#newPassword").click(()=>{
+                let password = $("#password").val()
+                let password2 = $("#password2").val()
+                if(password.length > 7 && password2.length >7){
+                    if(password === password2){
+                        $("#formulaire").submit()
+                    }else{
+                        alert(<?php echo $errorChangePassword ?>)
+                    }
+                } else {
+                    alert(<?php echo $errorChangePassword2 ?>)
+                }
+            })
+            let success = <?php echo $successRequest; ?>;
+            if (success == 1){ 
+               setTimeout(function (){window.location = "https://games-online.herokuapp.com/form.php" ; alert('<?php echo $passwordChanged?>')}, 200)
+            } 
         </script>
     </body>
 </html>    
