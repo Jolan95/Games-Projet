fe261968131a61e691aae987ccf34118e57a0fb5
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-07-04 18:44:23 +0200

    cleaning style  website

--- dice-game/jeu.php
+++ dice-game/jeu.php
@@ -1,9 +1,21 @@
-
-<?php
-$style="style.css";
-include '../header.php';
+<!DOCTYPE html>
+<?php 
+session_start();
+include "handleLanguage/lang.php";     
 ?>
-    <link rel="stylesheet" type="text/css" href="jeuDice.css">
+<html lang="fr">
+    <head>
+		<link rel="icon" type="image/png" href="Img/favicon.png">
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>J-Games Online</title>
+		<meta name="description" content="Jeux d'arcades, quiz, jeux de dés,... Venez découvrir notre liste de jeux !" />
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="Style/style.css">
+        <link rel="stylesheet" type="text/css" href="jeuDice.css">
+	</head>
+	<body>
+        <?php require_once 'header.php'; ?>
 <body id="bod">
 <input type=hidden id="record" value=<?php if(isset($_SESSION['record_dice'])){ echo $_SESSION['record_dice']; }?>>
 <!--<div class="container-fluid bk">-->
