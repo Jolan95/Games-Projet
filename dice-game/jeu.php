12c0cad1661782ebda87bd227cfba85e163768b6
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-07-05 11:13:26 +0200

    cleaning style dice

--- dice-game/jeu.php
+++ dice-game/jeu.php
@@ -14,50 +14,53 @@ include "../handleLanguage/lang.php";
         <!-- <link rel="stylesheet" type="text/css" href="jeuDice.css"> -->
 		<link rel="stylesheet" type="text/css" href="../Style/style.css">
 	</head>
-	<body>
+	<body class="game-dice">
         <?php require_once '../header.php'; ?>
+        <h1 hidden>Jeux Dé</h1>
         <input type=hidden id="record" value=<?php if(isset($_SESSION['record_dice'])){ echo $_SESSION['record_dice']; }?>>
         <div class="row justify-content-center">
-            <h4 class="col-2 justify-self-center"> <em><?php if(isset($_SESSION['record_dice'])){ echo $highScoreWin.' : +'.$_SESSION['record_dice']; }?></em></h4>
+            <div class="col-2 justify-self-center"> 
+                <em><?php if(isset($_SESSION['record_dice'])){ echo $highScoreWin.' : +'.$_SESSION['record_dice']; }?>
+                </em>
+            </div>
         </div>
         <div class="row">
-            <div class="col-5 text-center">
-               <h2 ><?php echo $player1 ?>   
-               <svg id="p1"xmlns="http://www.w3.org/2000/svg" width="3vh" fill="currentColor" style="color : red" class=" bi bi-circle-fill align-self-center" viewBox="0 0 16 16"  ><circle cx="8" cy="8" r="8"/></svg></h2>
+            <div class="col-2 offset-1 text-center">
+               <h2><?php echo $player1 ?>   
+               <svg id="p1"xmlns="http://www.w3.org/2000/svg" height="16px" width="16px" fill="currentColor" class="red" class=" bi bi-circle-fill align-self-center" viewBox="0 0 16 16"  ><circle cx="8" cy="8" r="8"/></svg></h2>
             </div>
             
-            <div class=" offset-2 col-5 text-center">
-                <h2 style="color : white">
+            <div class=" offset-6 col-2 text-center">
+                <h2 class="white">
                     <span id="textp"><?php echo $player2 ?> </span>
-                    <svg id="p2"   xmlns="http://www.w3.org/2000/svg" width="3vh" fill="currentColor"  style="color : red" class="bi bi-circle-fill align-self-center" viewBox="0 0 16 16" ><circle cx="8" cy="8" r="8"/></svg>
+                    <svg id="p2"   xmlns="http://www.w3.org/2000/svg"  height="16px" width="16px" fill="currentColor"  class="red" class="bi bi-circle-fill align-self-center" viewBox="0 0 16 16" ><circle cx="8" cy="8" r="8"/></svg>
                 </h2>
             </div>
         </div>
-
         <div class="row">
-            <div id="score1" class="col-5 col-ms-4  score text-center align-self-center sized">
+            <div id="score1" class="col-2 offset-1 text-center align-self-center ">
             </div>
 
-            <div class="col-2 col-ms-4 light text-center dice " id='dice'>  
+            <div class="offset-2 col-2 light text-center dice" id='dice'>  
             </div>
                         
-            <div id="score2" class="col-5  col-ms-4 score text-center align-self-center sized">0</div>
+            <div id="score2" class="col-5  col-ms-4 text-center align-self-center">0</div>
             </div>
         
-            <div class="row " style="height : 40vh">
-                <div id="current1" class="col-5  text-center align-self-center ">
-                    <div class="text-center boxcurr"style="background-color : lightblue; height : 17vh; width : 20vw">
+            <div class="row ">
+                <div id="current1" class="col-2 offset-1  text-center align-self-center ">
+                    <div class="text-center box-current">
                         <div>
                             <?php echo $current ?>
                         </div>
-                        <div id="scoreCurr1" class="numCurr" style="color : white">
+                        <div id="scoreCurr1" class="score-current" class="white">
                     </div>
                 </div>
             </div>
-            <div class="butt  col-2  text-center align-self-center">
+            <div class="col-2 col-offset-2  text-center align-self-center">
                 <div>
                     <button id="roll">
-                        <svg style="color : green;  "xmlns="http://www.w3.org/2000/svg" width="5vh" height="5vh" fill="currentColor" class="svgw bi bi-arrow-repeat" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="14px" height="14px" fill="currentColor" class="svgw bi bi-arrow-repeat" viewBox="0 0 16 16">
                             <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/>
                             <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/>
                         </svg>
@@ -65,8 +68,8 @@ include "../handleLanguage/lang.php";
                     </button>
                 </div>
                 <div>    
-                    <button type="button" id="hold"style="margin-top : 3vh">
-                        <svg style="color : red;  margin-bottom : 1.5vh" xmlns="http://www.w3.org/2000/svg"  width="5vh" height="5vh" fill="currentColor" class=" svgw bi bi-box-arrow-in-down" viewBox="0 0 16 16">
+                    <button type="button" id="hold">
+                        <svg xmlns="http://www.w3.org/2000/svg"  width="14px" height="14px" fill="currentColor" class=" svgw bi bi-box-arrow-in-down" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1h-2z"/>
                             <path fill-rule="evenodd" d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
                         </svg>
@@ -75,11 +78,11 @@ include "../handleLanguage/lang.php";
                 </div>
             </div>
             <div id="current2" class="col-5  just align-self-center">
-                <div  class="text-center  boxcurr"style="background-color : lightpink; height : 17vh; width : 20vw">
+                <div  class="text-center  box-current">
                     <div>
                         <?php echo $current ?>
                     </div>
-                    <div id="scoreCurr2" class="numCurr" style="color : white">
+                    <div id="scoreCurr2" class="score-current" class="white">
                     </div>
                 </div>
             </div>  
