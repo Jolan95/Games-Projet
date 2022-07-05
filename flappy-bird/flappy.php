a1fc9efc31317b8bfd28d550f66f94624bb29e7e
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-07-05 18:28:38 +0200

    redesign and clean codee game-dice

--- flappy-bird/flappy.php
+++ flappy-bird/flappy.php
@@ -12,21 +12,16 @@ include "../handleLanguage/lang.php";
 		<meta name="description" content="Jeux d'arcades, quiz, jeux de dés,... Venez découvrir notre liste de jeux !" />
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href="../Style/style.css">
-        <style>       
-            canvas{
-                border: 1px solid #000;
-                display: block;
-                margin: 0 auto;
-            }
-        </style>
     </head>
     <body>
         <?php require_once '../header.php'; ?>
-        <input type="text" hidden id="record" value=<?php if(isset($_SESSION['user'])){echo $_SESSION["record_flappy"];} else {echo 0;}?> >
-        <div class="text-center">
-            <h1 class="white">Flappy Bird</h1>
+        <div class="flappy">
+            <input type="text" hidden id="record" value=<?php if(isset($_SESSION['user'])){echo $_SESSION["record_flappy"];} else {echo 0;}?> >
+            <div class="text-center">
+                <h1 class="white">Flappy Bird</h1>
+            </div>
+            <canvas id="bird" width="320" height="480"></canvas>
         </div>
-        <canvas id="bird" width="320" height="480"></canvas>
 
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
         <script >
