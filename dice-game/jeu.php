b01b2fb4a38784908a2905873ae4f661ecc1c27e
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-07-05 17:19:30 +0200

    cleaning style game-dice

--- dice-game/jeu.php
+++ dice-game/jeu.php
@@ -7,7 +7,7 @@ include "../handleLanguage/lang.php";
     <head>
 		<link rel="icon" type="image/png" href="Img/favicon.png">
         <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 		<title>J-Games Online</title>
 		<meta name="description" content="Jeux d'arcades, quiz, jeux de dés,... Venez découvrir notre liste de jeux !" />
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
