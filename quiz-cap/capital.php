f3fbca72083860ee47b247e41f01976598727ebb
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-07-06 16:27:23 +0200

    redesign and clean code capitals

--- quiz-cap/capital.php
+++ quiz-cap/capital.php
@@ -105,7 +105,7 @@ include 'Class/user.php';
             }
 
             function hiddingCircles(){
-                circles.forEach(element => element.hidden = true)
+                circles.forEach(element => element.classList.add("d-none") )
             }
 
             function shuffle(array) {
