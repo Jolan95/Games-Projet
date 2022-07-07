973f68108427739f7c0210dcf23b2812c3136a74
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-07-08 01:42:15 +0200

    redesign and clean code capitals

--- PFC/pfc.php
+++ PFC/pfc.php
@@ -15,22 +15,23 @@ include "../handleLanguage/lang.php";
 	<body>
 		<?php include_once '../header.php'; ?>
 		<div class="pfc">
-			<h1 class="text-center"style="color : white"><?php echo $rpc ?></h1>
-			<div class="row scores d-flex justify-content-around">
-        		<div class=" score2">
-            	    <div id="score1" class="text-center">
-            	       0
-            	    </div>
-				</div>
-				<div>
-					<h2 id="sentences"><?php echo $makeYourChoice ?> </h2>
-				</div>
-       			<div class="score1">
-        		    <div id="score2" class="text-center">
-        		        0
-        		    </div>
-        		</div>
-			</div>	
+			<div class="container-fluid">
+				<h1 class="text-center"style="color : white"><?php echo $rpc ?></h1>
+				<div class="row scores d-flex justify-content-around">
+        			<div class=" score2">
+            	    	<div id="score1" class="text-center">
+            	    	   0
+            	    	</div>
+					</div>
+					<div>
+						<h2 id="sentences"><?php echo $makeYourChoice ?> </h2>
+					</div>
+       				<div class="score1">
+        			    <div id="score2" class="text-center">
+        			        0
+        			    </div>
+        			</div>
+				</div>	
 	
 				<div class="row my-3">
 					<div id="choix1"class="col-4 col-sm-3 offset-1 offset-sm-2 choices">
@@ -52,95 +53,95 @@ include "../handleLanguage/lang.php";
 		</div>
 		<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 		<script >
-    $(document).ready(()=>{
-  		let score1 = 0
-  		let score2 = 0
-		function win(){
-		  	score1++
-		  	$("h2").text(<?php  echo $won ?>+" !!!")
-		  	$("#choix1").css('border', '1vh solid green')
-		  	$("#choix2").css('border', '1vh solid red')
-		  	$('h2').css('color', 'green')
-		}
-		function tied(){
-		  	$("h2").text(<?php echo $tied ?>+" !")
-		  	$("#choix1").css('border', '1vh solid yellow')
-		  	$("#choix2").css('border', '1vh solid yellow')
-		  	$('h2').css('color', 'darkyellow')
-		}
-		function lose(){
-		  	score2++
-		  	$("#choix1").css('border', '1vh solid red')
-		  	$("#choix2").css('border', '1vh solid green')
-		  	$("h2").text(<?php echo $lost ?>+" !")
-		  	$('h2').css('color', 'red')
-		} 
-		function displayScore(){
-		    $('#score1').text(score1)
-		    $('#score2').text(score2);
-		    $('#choix1').text("")
-		    $('#choix2').text("")
-		}
-		function getRandom(){
-		  	return Math.floor(Math.random()* 3)
-		}
-		$('button').click(function(){
-			let ia = getRandom();
-			$("#choix1").removeClass('pierre')
-			$("#choix1").removeClass('ciseaux')
-			$("#choix1").removeClass('papier')
-			$('#choix1').addClass($(this).attr("id"));
-			let choix = $(this).attr("id");
-			switch(ia){
-			  	case 0:
-			    	$("#choix2").removeClass('pierre')
-			    	$("#choix2").removeClass('ciseaux')
-			    	$("#choix2").removeClass('papier')
-			    	$("#choix2").addClass('pierre');
-			    break;
-			  	case 1 : 
-		      		$("#choix2").removeClass('pierre')
-		      		$("#choix2").removeClass('ciseaux')
-		      		$("#choix2").removeClass('papier')
-		      		$("#choix2").addClass('papier');
-		      	break;
-		      	case 2 : 
-		      		$("#choix2").removeClass('pierre')
-		      		$("#choix2").removeClass('ciseaux')
-		      		$("#choix2").removeClass('papier')
-		      		$("#choix2").addClass('ciseaux');
-		      	break;
-		    }
-		    if(choix === "pierre" && ia === 0){
-		      	tied()
-		    }
-		    if(choix === "papier" && ia === 0){
-		      	win()
-		    }
-		    if(choix === "ciseaux" && ia === 0){
-		      	lose()
-		    }
-		    if(choix === "pierre" && ia === 1){
-		      	lose()
-		    }
-		    if(choix === "papier" && ia === 1){
-		      	tied()
-		    }
-		    if(choix === "ciseaux" && ia === 1){
-		      	win()
-		    }
-		    if(choix === "pierre" && ia === 2){
-		      	win()
-		    }
-		    if(choix === "papier" && ia === 2){
-		      	lose()
-		    }
-		    if(choix === "ciseaux" && ia === 2){
-		      	tied()
-		    }
-		    displayScore()
+    	$(document).ready(()=>{
+  			let score1 = 0
+  			let score2 = 0
+			function win(){
+			  	score1++
+			  	$("h2").text(<?php  echo $won ?>+" !!!")
+			  	$("#choix1").css('border', '1vh solid green')
+			  	$("#choix2").css('border', '1vh solid red')
+			  	$('h2').css('color', 'green')
+			}
+			function tied(){
+			  	$("h2").text(<?php echo $tied ?>+" !")
+			  	$("#choix1").css('border', '1vh solid yellow')
+			  	$("#choix2").css('border', '1vh solid yellow')
+			  	$('h2').css('color', 'darkyellow')
+			}
+			function lose(){
+			  	score2++
+			  	$("#choix1").css('border', '1vh solid red')
+			  	$("#choix2").css('border', '1vh solid green')
+			  	$("h2").text(<?php echo $lost ?>+" !")
+			  	$('h2').css('color', 'red')
+			} 
+			function displayScore(){
+			    $('#score1').text(score1)
+			    $('#score2').text(score2);
+			    $('#choix1').text("")
+			    $('#choix2').text("")
+			}
+			function getRandom(){
+			  	return Math.floor(Math.random()* 3)
+			}
+			$('button').click(function(){
+				let ia = getRandom();
+				$("#choix1").removeClass('pierre')
+				$("#choix1").removeClass('ciseaux')
+				$("#choix1").removeClass('papier')
+				$('#choix1').addClass($(this).attr("id"));
+				let choix = $(this).attr("id");
+				switch(ia){
+				  	case 0:
+				    	$("#choix2").removeClass('pierre')
+				    	$("#choix2").removeClass('ciseaux')
+				    	$("#choix2").removeClass('papier')
+				    	$("#choix2").addClass('pierre');
+				    break;
+				  	case 1 : 
+			      		$("#choix2").removeClass('pierre')
+			      		$("#choix2").removeClass('ciseaux')
+			      		$("#choix2").removeClass('papier')
+			      		$("#choix2").addClass('papier');
+			      	break;
+			      	case 2 : 
+			      		$("#choix2").removeClass('pierre')
+			      		$("#choix2").removeClass('ciseaux')
+			      		$("#choix2").removeClass('papier')
+			      		$("#choix2").addClass('ciseaux');
+			      	break;
+			    }
+			    if(choix === "pierre" && ia === 0){
+			      	tied()
+			    }
+			    if(choix === "papier" && ia === 0){
+			      	win()
+			    }
+			    if(choix === "ciseaux" && ia === 0){
+			      	lose()
+			    }
+			    if(choix === "pierre" && ia === 1){
+			      	lose()
+			    }
+			    if(choix === "papier" && ia === 1){
+			      	tied()
+			    }
+			    if(choix === "ciseaux" && ia === 1){
+			      	win()
+			    }
+			    if(choix === "pierre" && ia === 2){
+			      	win()
+			    }
+			    if(choix === "papier" && ia === 2){
+			      	lose()
+			    }
+			    if(choix === "ciseaux" && ia === 2){
+			      	tied()
+			    }
+			    displayScore()
+			})
 		})
-	})
-	</script>
-</body>
+		</script>
+	</body>
 </html>
