121569a1827673c2efe769635ad636465509b499
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-07-08 01:28:35 +0200

    redesign and clean code capitals

--- PFC/pfc.php
+++ PFC/pfc.php
@@ -17,13 +17,13 @@ include "../handleLanguage/lang.php";
 		<div class="pfc">
 			<h1 class="text-center"style="color : white"><?php echo $rpc ?></h1>
 			<div class="row scores d-flex justify-content-around">
-        		<div class=" col-2 score score2">
+        		<div class="score score2">
             	    <div id="score1" class=" col-6 align-self-center text-center">
             	       0
             	    </div>
 				</div>
 				<div>
-					<h2 id="sentences" class="color : white"><?php echo $makeYourChoice ?> </h2>
+					<h2 id="sentences"><?php echo $makeYourChoice ?> </h2>
 				</div>
        			<div class="score1 score">
         		    <div id="score2" class="col-6 text-center">
