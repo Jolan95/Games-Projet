4303af095a777edf7e5d984753daaa705640068d
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-07-08 00:46:38 +0200

    redesign and clean code capitals

--- PFC/pfc.php
+++ PFC/pfc.php
@@ -35,7 +35,7 @@ include "../handleLanguage/lang.php";
         		    </div>
         		</div>	
 				<div class="row my-3">
-					<div id="choix1"class="col-4 col-sm-3 offset-1 offset-sm-2 choices">
+					<div id="choix1"class="col-4 col-sm-3 offset-1 offset-sm-2 choices d-flex text-align-center">
 						?
 					</div>
 					<div id="choix2" class=" col-4 col-sm-3 offset-1 offset-sm-2 choices">
