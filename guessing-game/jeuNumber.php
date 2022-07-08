4cc4d9c51ab37333248c3b219feee1ab5eeb55a7
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-07-08 15:57:25 +0200

    redesign and clean code pfc and capitals

--- guessing-game/jeuNumber.php
+++ guessing-game/jeuNumber.php
@@ -52,8 +52,10 @@ include "../handleLanguage/lang.php";
         require_once '../header.php';
         ?>
         <div class="guess-number">
-            <div class="row box">
-                <div class="col-1 col-md-3"></div>
+            <div class="container">
+
+                <div class="row box">
+                    <div class="col-1 col-md-3"></div>
                 <div class="col-10 col-md-6 cell">
                     <div>
                         
@@ -80,9 +82,10 @@ include "../handleLanguage/lang.php";
         </div>
         <div class="col-1 col-md-3"></div>
     </div>
+</div>
     
-    </div>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous">
+</div>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous">
         </script>
 
 <script>
