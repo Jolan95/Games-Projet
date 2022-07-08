26df951c07be51aebc8f30b52b0472f4b34403b5
Jolan Aubry <jolan.aubry@hotmail.fr>
2022-07-08 19:47:10 +0200

    redesign and clean code jeuNumber

--- guessing-game/jeuNumber.php
+++ guessing-game/jeuNumber.php
@@ -30,10 +30,10 @@ include "../handleLanguage/lang.php";
                     </div>
                     <div class="d-flex justify-content-center my-3">
                         <input type="number"  id="guess">
-                        <button id="validate" class="btn btn-primary"><?php echo $valid?></button>
+                        <button id="validate" class="btn btn-success"><?php echo $valid?></button>
                         <div id="links" class="d-none">
                             <a href="https://games-online.herokuapp.com/GuessingGame/jeuNumber.php"><button id="restart" class="btn btn-primary" ><?php echo $restart ?></button></a>
-                            <a href="https://games-online.herokuapp.com/index.php"><button type="button"><?php echo $redirectToMenu ?></button></a>
+                            <a href="https://games-online.herokuapp.com/index.php"><button type="button" class="btn btn-dander"><?php echo $redirectToMenu ?></button></a>
                         </div>
                     </div>
                     <div class='mt-3 d-flex justify-content-between'>
